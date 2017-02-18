ba4b4d0a521dc9312e2b5eb19c579b2a6fb42f71
builder@ <nomadbyte@users.noreply.github.com>
2017-02-18 15:09:44 -0600

    build-0.2

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,5 +5,16 @@ echo "HOME=$HOME"                         ## /home/travis
 echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR" ## $HOME/build/<user>/<repo>
 pwd
 
-tar czvf $TRAVIS_BUILD_DIR/release.tar.gz .travis.yml scripts/
+wget https://github.com/rjperrella/jenkins-fossil-adapter/archive/master.tar.gz --output-document source.tar.gz
+mkdir source && tar xvf source.tar.gz --strip-components=1 -C source
+rm -rf source.tar.gz
+cd source
+
+cd fossil
+mvn clean verify
+
+ls -al $TRAVIS_BUILD_DIR/source/fossil/target
+
+cd $TRAVIS_BUILD_DIR/source/fossil/target
+tar czvf $TRAVIS_BUILD_DIR/release.tar.gz fossil.jar fossil.hpi
 

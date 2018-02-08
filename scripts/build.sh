426e1c98091270d6bddbdd6c220ec54765fa976b
nomadbyte <nomadbyte@users.noreply.github.com>
2018-02-08 00:29:29 -0600

    Test CI

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,57 @@
-#!/bin/bash
-set -ex
+#!/bin/bash -ex
 
 echo "HOME=$HOME"                         ## /home/travis
 echo "TRAVIS_BUILD_DIR=$TRAVIS_BUILD_DIR" ## $HOME/build/<user>/<repo>
 pwd
 
-tar czvf $TRAVIS_BUILD_DIR/release.tar.gz .travis.yml scripts/
+## get sources
+#mkdir sources
+#cd sources
+#
+#rm -f sources.tar.gz
+#
+#wget -O sources.tar.gz https://github.com/nomadbyte/fredcpp/archive/master.tar.gz
+#tar xvf sources.tar.gz
+#if [ ! -d fredcpp-master ]
+#then
+#  echo "E|NOSOURCES - fredcpp source directory not found." >>&2
+#  exit 1
+#fi
+#
+#cd fredcpp-master
+
+## setup env
+
+[ -s api.key ] || echo "$FRED_API_KEY" > api.key
+
+if [ ! -s api.key ]
+then
+  echo "E|NOAPIKEY - 'api.key' file is missing or empty." >>&2
+  exit 1
+fi
+
+
+## CURL CA root certificates
+##   curl-config --ca  OR curl -v --cacert no-such-file https://google.com
+CURL_CA_CERT=/etc/ssl/certs/ca-certificates.crt
+[ -s cacert.pem ] || cp  "$CURL_CA_CERT" cacert.pem
+
+if [ ! -s cacert.pem ]
+then
+  echo "E|NOCACERTS - 'cacert.pem' file is missing or empty." >>&2
+  exit 1
+fi
+
+
+[ ! -d build ] && mkdir build
+cd build
+
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=RelWithDebInfo ..
+
+## build
+
+make clean all
+make test
+
+ctest -V
 
---
+++ scripts/dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/bash -e
+
+sudo apt-get update
+sudo apt-get install doxygen
+
